475eafe84ece1ce321d9edbc68d5e7826033c0bf
jonathan <jonobral@gmail.com>
2020-04-20 23:21:53 -0600

    Pushed to chapter 34

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,33 @@
-require('dotenv').config({ path: '.env'});
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config({ path: '.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL
+server.express.use(cookieParser());
+
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
   }
-}, deets => {
-  console.log(`Server is running on port http:localhost${deets.port}`);
-})
+  next();
+});
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  }
+);
